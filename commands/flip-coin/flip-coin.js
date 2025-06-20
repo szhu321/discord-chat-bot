0859b9a50648a4c4b6d259749cbdccc6cd05d271
Sheng Wei Zhu <shengwei321@gmail.com>
2025-06-20 13:41:50 -0400

    Added a flip-coin command.

---
+++ commands/flip-coin/flip-coin.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("flip-coin")
+        .setDescription("Flips a coin. 50/50 for heads or tails."),
+    async execute(interaction) {
+        const value = Math.random();
+        let result = "Tails";
+        if(value > 0.5) {
+            result = "Heads";
+        }
+        await interaction.reply(`You got ${result}!`);
+    },
+}
